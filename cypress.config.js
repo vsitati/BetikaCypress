514e0d6ac5845896ee9e2d9570b1bc1aca925943
vsitati <vsitati@silkroadtech.com>
2022-09-28 14:37:15 +0300

    modified:   cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,8 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
-module.exports = defineConfig ({
+module.exports = defineConfig({
+
   e2e: {
-    setupNodeEvents(on, config) {
-    baseUrl:'https://www.betika.com/en-ke/login',
-    reporter;'mochawesome',
-    reporterOptions; {
-      reportDir: 'cypress/results',
-      overwrite; false,
-      html; true,
-      json; false
-      reportFilename: '[status]_[datetime]-[name]-report',
-      timestamp; 'longDate'
-    }
-    }
+    baseUrl: 'https://www.betika.com/en-ke/login'
   }
 })
